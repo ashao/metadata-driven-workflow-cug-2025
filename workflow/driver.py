d8f98b7dc003f35bdabc7f85436a869817d249e0
Andrew Shao <Andrew.Shao@hpe.com>
2025-03-31 17:02:38 +0000

    Add ability to configure a low-res sim

--- workflow/driver.py
+++ workflow/driver.py
@@ -1,6 +1,9 @@
+from pathlib import Path
+
 from smartsim import Experiment
 
 MOM6_NPROCS = 24
+RESTART_PATH = Path("/lustre/data/shao/cug_2024/restart")
 
 def main():
 
@@ -24,27 +27,43 @@ def main():
     sampler = exp.create_model("data_sampler", rs_sampler)
     sampler.attach_generator_files(to_copy="./out_of_sample_worker.py")
 
-    rs_mom6 = exp.create_run_settings("MOM6_inputs/MOM6", run_command="mpirun")
-    rs_mom6.set_tasks(MOM6_NPROCS)
-    mom6 = exp.create_model("MOM6", rs_mom6, params={
+    rs_mom6_high_res = exp.create_run_settings("MOM6_inputs/MOM6", run_command="mpirun")
+    rs_mom6_high_res.set_tasks(MOM6_NPROCS)
+    mom6_high_res = exp.create_model("MOM6", rs_mom6_high_res, params={
         "DAYMAX": 2191,
         "NIGLOBAL": 240,
-        "NJGLOBAL": 320
+        "NJGLOBAL": 320,
+        "DT": 300.0,
+        "DT_THERM": 900.0,
+        "MEKE_TRAINING": ".true.",
         }
     )
-    mom6.attach_generator_files(to_copy="MOM6_inputs/Phillips_2layer", to_configure=["MOM6_inputs/MOM_override"])
+    mom6_high_res.attach_generator_files(
+        to_copy="MOM6_inputs/Phillips_2layer",
+        to_configure=["MOM6_inputs/MOM_override"],
+        to_symlink=str(RESTART_PATH / "high_res" / "INPUT")
+    )
+
+    rs_mom6_low_res = exp.create_run_settings("MOM6_inputs/MOM6", run_command="mpirun")
+    rs_mom6_low_res.set_tasks(MOM6_NPROCS)
+    mom6_low_res = exp.create_model("MOM6", rs_mom6_low_res, params={
+        "DAYMAX": 2191,
+        "NIGLOBAL": 90,
+        "NJGLOBAL": 120,
+        "DT": "900.0",
+        "DT_THERM": "1800.0",
+        "MEKE_TRAINING": "False",
+        }
+    )
+    mom6_low_res.attach_generator_files(to_copy="MOM6_inputs/Phillips_2layer", to_configure=["MOM6_inputs/MOM_override"])
 
     # Create and configure the database
     db = exp.create_database(interface="lo")
 
     try:
         exp.start(db)
-        exp.generate(mom6, sampler, overwrite=True)
-        # TODO: cmf: log the input files for the
-
-        exp.start(mom6, sampler, block=True)
-
-        # TODO: cmf call to log the restart file and other
+        exp.generate(mom6_high_res, sampler, overwrite=True)
+        exp.start(mom6_high_res, sampler, block=True)
     finally:
         exp.stop(db)
 
